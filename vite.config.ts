24954dca50b043305be5ccf7300bafabbfb93939
Cupid Valentine <westerngod@126.com>
2024-01-05 00:07:42 +0800

    chore: config vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import { createRequire } from 'node:module'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5'
 
+const require = createRequire(import.meta.url)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS(), ckeditor5({ theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' )})],
+  plugins: [vue(), UnoCSS(), ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') })],
+  base: '/ckeditor-vue3-starter/',
 })
