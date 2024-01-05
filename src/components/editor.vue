b1bb534c7a88686fb195aaf51c8a76ac4b0e4ec5
Cupid Valentine <westerngod@126.com>
2024-01-05 23:27:51 +0800

    refactor: optimized codes

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,73 +1,33 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-// import type { EditorConfig } from '@ckeditor/ckeditor5-core'
 import CKEditor from '@ckeditor/ckeditor5-vue'
+import '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn'
+
+// import type { EditorConfig } from '@ckeditor/ckeditor5-core'
+
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic'
-import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder'
-import { Alignment } from '@ckeditor/ckeditor5-alignment'
+
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat'
-import { Autosave } from '@ckeditor/ckeditor5-autosave'
-import {
-  Bold,
-  Code,
-  Italic,
-  Strikethrough,
-  Subscript,
-  Superscript,
-  Underline,
-} from '@ckeditor/ckeditor5-basic-styles'
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles'
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote'
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services'
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block'
+import type { EditorConfig } from '@ckeditor/ckeditor5-core'
 import { Essentials } from '@ckeditor/ckeditor5-essentials'
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace'
-import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font'
-import { Heading, Title } from '@ckeditor/ckeditor5-heading'
-import { Highlight } from '@ckeditor/ckeditor5-highlight'
-import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line'
-import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed'
+import { Heading } from '@ckeditor/ckeditor5-heading'
 import {
-  DataFilter,
-  DataSchema,
-  GeneralHtmlSupport,
-  HtmlComment,
-} from '@ckeditor/ckeditor5-html-support'
-import {
-  AutoImage,
   Image,
   ImageCaption,
-  ImageInsert,
   ImageResize,
   ImageStyle,
   ImageToolbar,
   ImageUpload,
 } from '@ckeditor/ckeditor5-image'
-import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent'
-import { TextPartLanguage } from '@ckeditor/ckeditor5-language'
-import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link'
-import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list'
-import { Markdown } from '@ckeditor/ckeditor5-markdown-gfm'
-import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed'
-import { Mention } from '@ckeditor/ckeditor5-mention'
-import { PageBreak } from '@ckeditor/ckeditor5-page-break'
+import { Indent } from '@ckeditor/ckeditor5-indent'
+import { Link } from '@ckeditor/ckeditor5-link'
+import { List } from '@ckeditor/ckeditor5-list'
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed'
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph'
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office'
-import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format'
-import { StandardEditingMode } from '@ckeditor/ckeditor5-restricted-editing'
-import { SelectAll } from '@ckeditor/ckeditor5-select-all'
-import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks'
-import { SourceEditing } from '@ckeditor/ckeditor5-source-editing'
-import {
-  SpecialCharacters,
-  SpecialCharactersArrows,
-  SpecialCharactersCurrency,
-  SpecialCharactersEssentials,
-  SpecialCharactersLatin,
-  SpecialCharactersMathematical,
-  SpecialCharactersText,
-} from '@ckeditor/ckeditor5-special-characters'
-import { Style } from '@ckeditor/ckeditor5-style'
 import {
   Table,
   TableCaption,
@@ -78,11 +38,7 @@ import {
 } from '@ckeditor/ckeditor5-table'
 import { TextTransformation } from '@ckeditor/ckeditor5-typing'
 import { Undo } from '@ckeditor/ckeditor5-undo'
-import '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn'
-
-// import { EditorWatchdog } from '@ckeditor/ckeditor5-watchdog'
-import { WordCount } from '@ckeditor/ckeditor5-word-count'
-import type { EditorConfig } from '@ckeditor/ckeditor5-core'
+import FullScreen from '@pikulinpw/ckeditor5-fullscreen'
 
 const props = withDefaults(defineProps<{ preview?: boolean }>(), { preview: false })
 
@@ -93,8 +49,6 @@ const toolbar = computed(() => {
 
   return {
     items: [
-      'heading',
-      '|',
       'bold',
       'italic',
       'link',
@@ -110,47 +64,38 @@ const toolbar = computed(() => {
       'mediaEmbed',
       'undo',
       'redo',
-      'alignment',
-      'code',
-      'codeBlock',
-      'findAndReplace',
-      'fontBackgroundColor',
-      'fontColor',
-      'fontFamily',
-      'fontSize',
-      'highlight',
-      'horizontalLine',
-      'htmlEmbed',
-      'imageInsert',
-      'pageBreak',
-      'removeFormat',
-      'selectAll',
-      'showBlocks',
-      'sourceEditing',
-      'specialCharacters',
-      'restrictedEditingException',
-      'strikethrough',
-      'style',
-      'subscript',
-      'superscript',
-      'textPartLanguage',
-      'todoList',
-      'underline',
+      'heading',
+      'fullScreen',
     ],
   }
 })
 const editorConfig: EditorConfig = {
-  plugins: [Alignment, AutoImage, AutoLink, Autoformat, Autosave, BlockQuote, Bold, CloudServices, Code, CodeBlock, DataFilter, DataSchema, Essentials, FindAndReplace, FontBackgroundColor, FontColor, FontFamily, FontSize, GeneralHtmlSupport, Heading, Highlight, HorizontalLine, HtmlComment, HtmlEmbed, Image, ImageCaption, ImageInsert, ImageResize, ImageStyle, ImageToolbar, ImageUpload, Indent, IndentBlock, Italic, Link, LinkImage, List, ListProperties, Markdown, MediaEmbed, MediaEmbedToolbar, Mention, PageBreak, Paragraph, PasteFromOffice, RemoveFormat, SelectAll, ShowBlocks, SourceEditing, SpecialCharacters, SpecialCharactersArrows, SpecialCharactersCurrency, SpecialCharactersEssentials, SpecialCharactersLatin, SpecialCharactersMathematical, SpecialCharactersText, StandardEditingMode, Strikethrough, Style, Subscript, Superscript, Table, TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar, TextPartLanguage, TextTransformation, Title, TodoList, Underline, Undo, UploadAdapter, WordCount],
+  plugins: [Autoformat, BlockQuote, Bold, Essentials, Heading, Image, ImageCaption, ImageResize, ImageStyle, ImageToolbar, ImageUpload, Indent, Italic, Link, List, MediaEmbed, Paragraph, PasteFromOffice, Table, TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar, TextTransformation, Undo, FullScreen],
   toolbar: toolbar.value,
+
+  link: {
+    // Automatically add target="_blank" and rel="noopener noreferrer" to all external links.
+    addTargetToExternalLinks: true,
+
+    // Let the users control the "download" attribute of each link.
+    // decorators: [
+    //   {
+    //     mode: 'manual',
+    //     label: 'Downloadable',
+    //     attributes: {
+    //       download: 'download',
+    //     },
+    //   },
+    // ],
+  },
   language: 'zh-cn',
   image: {
     toolbar: [
-      'imageTextAlternative',
+     	'imageTextAlternative',
       'toggleImageCaption',
       'imageStyle:inline',
       'imageStyle:block',
       'imageStyle:side',
-      'linkImage',
     ],
   },
   table: {
@@ -163,10 +108,28 @@ const editorConfig: EditorConfig = {
     ],
   },
 }
+
+function onEditorReady() {}
+function onEditorFocus() {}
+function onEditorBlur() {}
+function onEditorInput() {}
+function onEditorDestroy() {}
 </script>
 
 <template>
-  <div><CKEditor.component v-model="editorData" :disabled="preview" :editor="ClassicEditor" :config="editorConfig" /></div>
+  <div>
+    <CKEditor.component
+      v-model="editorData"
+      :disabled="preview"
+      :editor="ClassicEditor"
+      :config="editorConfig"
+      @ready="onEditorReady"
+      @focus="onEditorFocus"
+      @blur="onEditorBlur"
+      @input="onEditorInput"
+      @destroy="onEditorDestroy"
+    />
+  </div>
 </template>
 
 <style scoped>
